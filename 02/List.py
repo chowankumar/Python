943686871fb3c077d9bf035dc4f12e1e1ad78934
chowankumar <chowanmalhani5@gmail.com>
2024-08-26 12:43:27 +0500

    complete the list

---
+++ 02/List.py
@@ -0,0 +1,24 @@
+team = ["a","b","c","d","e"]
+  
+print(team)
+print(team[0])
+print(team[-1])
+print(team[1:3])
+print(team[:3])
+
+
+team[3] = "three"
+
+team.append("last")
+
+team.pop()
+
+team.remove
+
+team.insert(2,"insert")
+
+#.copy() alg alg reference hoga memory mn.
+
+square_num = [x**2 for x in range(10)]
+print(square_num)
+
