78827182f5af031bad655cb0e4293a3a8d7a35e7
chowankumar <chowanmalhani5@gmail.com>
2024-08-26 12:08:03 +0500

    String done

---
+++ 01/basics.py
@@ -0,0 +1,10 @@
+h1 = [1,2,3]
+h2 = h1[:]
+print( h1)
+print(h2)
+
+h1[0] = 55
+print(h1)
+print(h2)
+
+# here we make the copy of the h1 in h2
---
+++ 02/String.py
@@ -0,0 +1,40 @@
+num = "123456789"
+print(num[:])
+print(num[3:])
+print(num[:7])
+print(num[0:7:2])
+
+string = "  Chowan Kumar  "
+
+print(string.len(string))
+
+print(string.lower())
+
+print(string.upper())
+
+print(string.strip())
+
+print(string.replace("kumar", "MAL"))
+
+#change string into the list
+print(string.split(", "))
+
+print(string.find("kumar"))
+
+print(string.count("kumar"))
+
+print("kumar" in string)
+
+
+
+name = "chowan"
+lastname = "kumar"
+sente = "my name is {} {}"
+print(sente.format(name,lastname))
+
+
+
+
+
+
+
