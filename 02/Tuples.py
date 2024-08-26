5def915998fa1ab239b324e9eac29aebe5488526
chowankumar <chowanmalhani5@gmail.com>
2024-08-26 15:06:16 +0500

    complete tuple

---
+++ 02/Tuples.py
@@ -0,0 +1,21 @@
+aphabets = ("a","b","c" )
+#it is immutable
+
+aphabetsadd = ("d","e")
+
+
+allaphabet = aphabets+aphabetsadd
+print(allaphabet)
+
+
+print(allaphabet.count("a"))
+
+
+
+#conditions
+if "d" in allaphabet :
+    print("yes d is available")
+
+#
+values = ("chowan","Amrat","bhavish")
+print(values)
